188848d9c25a14c858021c2508eff15c5317225e
Nurelmallah <nourhan.elmallah@gmail.com>
2023-08-10 11:08:12 -0700

    edit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-/* This is a C program that prints a message using puts */
-int main(void) { puts("Programming is like building a multilingual puzzle,"); return (0); }
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle,");
+    return 0;
+}
