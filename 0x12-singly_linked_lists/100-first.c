9cf292ffb02f1fed231ff9a6a58fa405c1c286fa
Nurelmallah <nourhan.elmallah@gmail.com>
2023-09-22 16:53:33 -0700

    edit 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,27 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * myStartupFun - Function to be executed before main.
- * It prints the desired message.
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
  */
-void myStartupFun(void) __attribute__((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
 
 /**
- * myStartupFun - Implementation of myStartupFun.
+ * myStartupFun - implementation of myStartupFun
  */
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - The main function.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A new adventure is about to unfold...)\n");
-	return (0);
 }
