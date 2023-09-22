5a4b99cb8d7c237b77569b1aba1f5172b5a3cb17
Nurelmallah <nourhan.elmallah@gmail.com>
2023-09-22 16:49:38 -0700

    add 100 +  edit lists.h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,28 @@
+Copy code
+#include <stdio.h>
+
+/**
+ * myStartupFun - Function to be executed before main.
+ * It prints the desired message.
+ */
+void myStartupFun(void) __attribute__((constructor));
+
+/**
+ * myStartupFun - Implementation of myStartupFun.
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - The main function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(A new adventure is about to unfold...)\n");
+	return (0);
+}
